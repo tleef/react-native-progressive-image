8c377783a3864e333aff079daec2b4968dc040f0
tleef <tom@tleef.com>
2019-09-15 12:38:07 -0300

    Add image caching

--- src/ProgressiveImage.tsx
+++ src/ProgressiveImage.tsx
@@ -1,7 +1,9 @@
+import Cache from "@tleef/expo-image-cache";
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 import Animated, { Easing } from "react-native-reanimated";
 
+const cache = new Cache();
 const { Value, timing } = Animated;
 
 interface ISource {
@@ -9,32 +11,168 @@ interface ISource {
 }
 
 interface IProps {
-  innerKey?: string;
+  noCache: boolean;
+  noPreview: boolean;
   imageUri?: string;
   imageSource?: ISource;
   previewUri?: string;
   previewSource?: ISource;
-  previewBlurRadius?: number;
+  previewBlurRadius: number;
+  animDuration: number;
   style?: any;
 }
 
-interface IContainerProps {
-  key?: string;
-  style: any;
+interface IState {
+  imageUri?: string;
 }
 
-interface IPreviewProps {
-  key?: string;
-  style: any;
-  blurRadius: number;
-  source: ISource;
-}
+export default class ProgressiveImage extends React.Component<IProps, IState> {
+  public static defaultProps = {
+    noCache: false,
+    noPreview: false,
+    previewBlurRadius: 2,
+    animDuration: 500,
+  };
 
-interface IImageProps {
-  key?: string;
-  style: any;
-  source: ISource;
-  onLoad: () => void;
+  private readonly opacity: Animated.Value<number>;
+  private readonly anim: Animated.BackwardCompatibleWrapper;
+  private animFinished: boolean;
+
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      imageUri: undefined,
+    };
+
+    this.opacity = new Value(0);
+    this.animFinished = false;
+    this.anim = timing(this.opacity, {
+      duration: props.animDuration,
+      toValue: 1,
+      easing: Easing.inOut(Easing.ease),
+    });
+
+    this.onImageLoad = this.onImageLoad.bind(this);
+    this.onAnimFinished = this.onAnimFinished.bind(this);
+  }
+
+  public componentDidMount() {
+    this.resolveImageUri();
+  }
+
+  public componentDidUpdate(prevProps: Readonly<IProps>): void {
+    let prevImageUri: string | undefined;
+    if (prevProps.imageUri) {
+      prevImageUri = prevProps.imageUri;
+    } else if (prevProps.imageSource) {
+      prevImageUri = prevProps.imageSource.uri;
+    }
+
+    let curImageUri: string | undefined;
+    if (this.props.imageUri) {
+      curImageUri = this.props.imageUri;
+    } else if (this.props.imageSource) {
+      curImageUri = this.props.imageSource.uri;
+    }
+
+    if (curImageUri !== prevImageUri) {
+      this.reset();
+      this.resolveImageUri();
+    }
+  }
+
+  public reset() {
+    this.opacity.setValue(0);
+    this.animFinished = false;
+    this.setState({
+      imageUri: undefined,
+    });
+  }
+
+  public async resolveImageUri() {
+    if (this.props.noCache) {
+      return;
+    }
+
+    let imageUri: string | undefined;
+    if (this.props.imageUri) {
+      imageUri = this.props.imageUri;
+    } else if (this.props.imageSource) {
+      imageUri = this.props.imageSource.uri;
+    }
+
+    if (!imageUri || !imageUri.startsWith("http")) {
+      this.setState({
+        imageUri,
+      });
+      return;
+    }
+
+    try {
+      const localImageUri = await cache.getLocalUriAsync(imageUri);
+      this.setState({
+        imageUri: localImageUri,
+      });
+    } catch (e) {
+      console.log(`Error while caching image, using remote URI: ${imageUri}`);
+      console.log(e);
+      this.setState({
+        imageUri,
+      });
+    }
+  }
+
+  public render() {
+    let {
+      noCache,
+      noPreview,
+      previewUri,
+      previewSource,
+      previewBlurRadius,
+      style,
+    } = this.props;
+
+    previewSource = previewUri ? { uri: previewUri } : previewSource;
+
+    let imageSource;
+    if (!noCache && this.state.imageUri) {
+      imageSource = { uri: this.state.imageUri };
+    } else if (noCache && this.props.imageUri) {
+      imageSource = { uri: this.props.imageUri };
+    } else if (noCache && this.props.imageSource) {
+      imageSource = this.props.imageSource;
+    }
+
+    return (
+      <View style={[styles.container, style]}>
+        {!noPreview && previewSource ? (
+          <Image
+            style={styles.image}
+            blurRadius={previewBlurRadius}
+            source={previewSource}
+          />
+        ) : null}
+        {imageSource ? (
+          <Animated.Image
+            style={[styles.image, { opacity: this.opacity }]}
+            source={imageSource}
+            onLoad={this.onImageLoad}
+          />
+        ) : null}
+      </View>
+    );
+  }
+
+  private onAnimFinished({ finished }: { finished: boolean }) {
+    this.animFinished = finished;
+  }
+
+  private onImageLoad() {
+    if (!this.animFinished) {
+      this.anim.start(this.onAnimFinished);
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -50,84 +188,3 @@ const styles = StyleSheet.create({
     position: "relative",
   },
 });
-
-const emptySource = { uri: "" };
-
-export default class ProgressiveImage extends React.PureComponent<IProps> {
-  private readonly opacity: Animated.Value<number>;
-  private readonly anim: Animated.BackwardCompatibleWrapper;
-  private animFinished: boolean;
-
-  constructor(props: any) {
-    super(props);
-
-    this.opacity = new Value(0);
-    this.animFinished = false;
-    this.anim = timing(this.opacity, {
-      duration: 500,
-      toValue: 1,
-      easing: Easing.inOut(Easing.ease),
-    });
-
-    this.onImageLoad = this.onImageLoad.bind(this);
-    this.onAnimFinished = this.onAnimFinished.bind(this);
-  }
-
-  public render() {
-    let {
-      innerKey,
-      imageUri,
-      imageSource,
-      previewUri,
-      previewSource,
-      previewBlurRadius,
-      style,
-    } = this.props;
-
-    imageSource = imageUri ? { uri: imageUri } : imageSource;
-    previewSource = previewUri ? { uri: previewUri } : previewSource;
-
-    const containerProps: IContainerProps = {
-      style: styles.container,
-    };
-
-    const previewProps: IPreviewProps = {
-      style: styles.image,
-      blurRadius: typeof previewBlurRadius === "number" ? previewBlurRadius : 2,
-      source: previewSource || emptySource,
-    };
-
-    const imageProps: IImageProps = {
-      style: [styles.image, { opacity: this.opacity }],
-      source: imageSource || emptySource,
-      onLoad: this.onImageLoad,
-    };
-
-    if (style) {
-      containerProps.style = [styles.container, style];
-    }
-
-    if (innerKey) {
-      containerProps.key = `${innerKey}-container`;
-      previewProps.key = `${innerKey}-preview`;
-      imageProps.key = `${innerKey}-image`;
-    }
-
-    return (
-      <View {...containerProps}>
-        {previewSource ? <Image {...previewProps} /> : null}
-        <Animated.Image {...imageProps} />
-      </View>
-    );
-  }
-
-  private onAnimFinished({ finished }: { finished: boolean }) {
-    this.animFinished = finished;
-  }
-
-  private onImageLoad() {
-    if (!this.animFinished) {
-      this.anim.start(this.onAnimFinished);
-    }
-  }
-}
